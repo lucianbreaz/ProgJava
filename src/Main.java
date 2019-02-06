013f763e390bd9d452157415cdb8139b56e8f35a
LucianBreaz <lucianbreaz4@gmail.com>
2019-02-06 21:02:28 +0200

    gf

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int adunare = 2 + 3;
+        System.out.println(adunare);
+        adunare++;      //adunare=adunare+1;
+        adunare += 3; //adunare=adunare+3
+        int scadere = 2 - 4546757;
+
+        System.out.println("Hello World!");
+    }
+}
